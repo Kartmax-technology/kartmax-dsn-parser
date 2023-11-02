47713501625a621545b88fa2df749753c71a7a19
Sagar Chauhan <coolsagar005@gmail.com>
2023-11-02 18:22:48 +0530

    updated desc

---
+++ src/LoadEnv.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LoadEnv;
+class LoadEnv
+{
+    public static function load() {
+        $envFile = __DIR__ . '/.env';
+        if (!file_exists($envFile)) {
+            throw new Exception('.env file does not exist.');
+        }
+
+        $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) {
+                continue; // Skip comments
+            }
+
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            if (!array_key_exists($name, $_ENV)) {
+                $_ENV[$name] = $value;
+                putenv("$name=$value");
+            }
+        }
+    }
+}
